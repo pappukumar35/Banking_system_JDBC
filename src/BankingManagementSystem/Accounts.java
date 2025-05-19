08fc704b88ff31e1084800ac27b12606652f1ee9
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2025-05-19 09:56:08 +0530

    Update Accounts.java

--- src/BankingManagementSystem/Accounts.java
+++ src/BankingManagementSystem/Accounts.java
@@ -3,14 +3,6 @@ package BankingManagementSystem;
 import java.sql.*;
 import java.util.Scanner;
 
-public class Accounts {
-    private Connection connection;
-    private Scanner scanner;
-    public Accounts(Connection connection, Scanner scanner){
-        this.connection = connection;
-        this.scanner = scanner;
-
-    }
 
 
 
